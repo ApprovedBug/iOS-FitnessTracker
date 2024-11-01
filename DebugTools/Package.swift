d5ede0d3dba26b5e85861a8591645790d3740bd0
Jack Moseley <approvedbug@outlook.com>
2024-11-01 20:03:00 +0000

    [bugfix] fix dependency typo

--- DebugTools/Package.swift
+++ DebugTools/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../ConfigurationManagement"),
-        .package(path: "../DependencyMangagement")
+        .package(path: "../DependencyManagement")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
